aece75b2b35843b975ec0255059131c648af6546
Murray Steele <muz@h-lame.com>
2017-07-20 13:41:51 +0000

    Performance improvements to removing the BOM
    1. Replace the `string.bytes[0..2] == [0xEF, 0xBB, 0xBF]` check with
    a string equality check on the first character of the string.  We need
    to make sure we're dealing with a utf8 string to perform this check
    correctly so we force the string to utf8 and do the check, but then set
    it back to its old encoding afterwards.  `String#force_encoding` modifies
    self so it would be bad-form if running remove_bom on an latin-1 encoded
    string resulted in it being encoded in utf-8 afterwards.
    
    2. Replace the `string.force_encoding('utf-8')[1..-1]` way of removing the
    first character of the string with one that a) doesn't modify the original
    string, and is faster (ironically by modifying the duplicate with
    `String#slice!`.  As `String#slice!` returns the deleted character instead
    of `self` we need to use `Object#tap` to make sure we return the modified
    string and not the bit of it we've sliced out.
    
    To support these changes we refactor the BomRemover to be a class instead
    of a module and wrap it up in a class method invoker to keep the API the
    same (e.g. `BomRemover.remove_bom(string)`).
    
    Both of these improvements were found by implementing many alternate
    implementations and running `Benchmark.bmbm` to find the fastest.  The
    original method of checking for the presence of the utf-8 string was
    much slower than others, and while we don't get a huge speed boost from
    our new bom removal method it is safer and still faster than some
    alternatives.
    
    See: https://gist.github.com/h-lame/e5ec2e5dcfa58abaccb693c28ed28f45
    for details of the benchmarking and alternate implementations we tried.

--- lib/asset_bom_removal/bom_remover.rb
+++ lib/asset_bom_removal/bom_remover.rb
@@ -1,12 +1,40 @@
 module AssetBomRemoval
-  module BomRemover
-    def remove_bom(css_string)
-      if css_string.bytes[0..2] == [0xEF, 0xBB, 0xBF]
-        css_string.force_encoding('UTF-8')[1..-1]
+  class BomRemover
+    def self.remove_bom(string)
+      new(string).remove_bom
+    end
+
+    def initialize(string)
+      @string = string
+    end
+
+    def remove_bom
+      if string_starts_with_utf8_bom?
+        string.dup.force_encoding('UTF-8').tap do |utf8_string|
+          utf8_string.slice!(0)
+        end
       else
-        css_string
+        string
       end
     end
-    module_function :remove_bom
+
+  private
+
+    attr_reader :string
+
+    def string_starts_with_utf8_bom?
+      with_encoding('UTF-8') do |utf8_string|
+        utf8_string[0] == "\xEF\xBB\xBF"
+      end
+    end
+
+    def with_encoding(encoding)
+      old_encoding = string.encoding
+      begin
+        return (yield string.force_encoding(encoding))
+      ensure
+        string.force_encoding(old_encoding)
+      end
+    end
   end
 end
